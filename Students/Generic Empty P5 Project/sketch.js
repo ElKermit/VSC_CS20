dcb6f5b86d4344ec13093e9fd216125b62689978
Andre <byte.it@hotmail.com>
2022-01-17 17:33:51 -0600

    Added Examples

---
+++ Students/Generic Empty P5 Project/sketch.js
@@ -0,0 +1,20 @@
+/*
+* Student Name
+* Project Name
+* Date
+* Description
+*/
+
+/** Global Variables */
+
+
+function setup() {
+  let sketch = createCanvas(600, 400);
+}
+
+
+function draw() {
+  background(220);
+
+  
+}
---
+++ Teacher/Unit_1_Example_2/sketch.js
@@ -0,0 +1,19 @@
+/*
+* CS 20
+* Unit 1
+* Example 2
+*/
+
+/** Global Variables */
+
+
+function setup() {
+  let sketch = createCanvas(600, 400);
+}
+
+
+function draw() {
+  background(220);
+
+  
+}
