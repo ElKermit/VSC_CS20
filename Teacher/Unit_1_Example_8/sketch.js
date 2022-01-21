820e116030ffdeef4dd5f5b422b1646c9b7a804a
Andre <byte.it@hotmail.com>
2022-01-20 22:50:05 -0600

    added unit 1 example 8

--- Teacher/Unit_1_Example_8/sketch.js
+++ Teacher/Unit_1_Example_8/sketch.js
@@ -1,43 +1,37 @@
 /*
 * CS 20
 * Unit 1
-* Example 7
+* Example 8
+* Live server extension required to load resources locally
 */
 
 /** Global Variables */
-let eventName = "Big Fish Concert";
-let preSales;
-let daySales;
-let prePrice = 15;
-let dayPrice = 20;
-let totalSales;
+let first, second;
+let transparency = 255;
+
+function preload() {
+  first = loadImage("./images/start.png");
+  second = loadImage("./images/end.png");
+}
 
 function setup() {
-  let sketch = createCanvas(600, 400);
-
-  background(150, 50, 50);
-  textSize(18);
-  preSales = 200;
-  daySales = window.prompt("How many tickets did we sell today?");
-  // window.alert("Total tickets sold is " +  (preSales + daySales) );
+  let sketch = createCanvas(300, 415);
+  imageMode(CENTER);
 }
 
 function draw() {
-  background(150, 250, 250);
-  textSize(40);
-  text(eventName, 50, 50);
-  textSize(18);
-  text("Presales: " + preSales, 50, 100);
-  text("Price $" + prePrice, 200, 100);
+  background(0);
+  tint(255, 255);
+  image(second, 150, 207, 300, 415);
+  tint(255, transparency);
+  image(first, 150, 207, 300, 415);
 
-  text("Today sales: " + daySales, 50, 125);
-  text("Price $" + dayPrice, 200, 125);
-
-  totalSales = preSales * prePrice + daySales * dayPrice;
-  text("Our total is $" + totalSales, 50, 200);
-
+  transparency -= 1;
+  if (transparency < 1) {
+    transparency = 255;
+  }
 }
 
-function keyPressed() {
-  daySales++;
+function mousePressed() {
+  transparency -= 5;
 }
