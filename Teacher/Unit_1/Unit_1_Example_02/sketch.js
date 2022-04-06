b9c15779eba81b7881fa4cd8617e2a823175241a
Andreas.Home <byte.it@hotmail.com>
2022-04-06 17:00:09 -0500

    added onMouse click to stop animation

--- Teacher/Unit_1/Unit_1_Example_02/sketch.js
+++ Teacher/Unit_1/Unit_1_Example_02/sketch.js
@@ -6,7 +6,7 @@
 
 /** Global Variables */
 let textX = 300;
-
+let isRunning = true;
 function setup() {
   let sketch = createCanvas(600, 400);
 }
@@ -15,14 +15,23 @@ function draw() {
   background(255, 0, 0);
 
   noStroke();
-  fill(255, 255,0);
+  fill(255, 255, 0);
   textSize(44);
   text("Welcome to CS20!", textX, 150);
+  if (isRunning == true) {
+    textX = textX - 2;
 
-  textX = textX - 2;
-
-  if (textX < -250) {
-    textX = 600;
-    print("too far right, restart on the left");
+    if (textX < -250) {
+      textX = 600;
+      print("too far right, restart on the left");
+    }
   }
+}
+
+function mousePressed() {
+  isRunning = false;
+}
+
+function mouseReleased(){
+  isRunning = true;
 }
