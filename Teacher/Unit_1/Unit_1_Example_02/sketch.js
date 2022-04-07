8cd12b464d749d53e60f4713016462ed39a90d9f
Andreas.Home <byte.it@hotmail.com>
2022-04-06 19:53:23 -0500

    added comment

--- Teacher/Unit_1/Unit_1_Example_02/sketch.js
+++ Teacher/Unit_1/Unit_1_Example_02/sketch.js
@@ -7,6 +7,7 @@
 /** Global Variables */
 let textX = 300;
 let isRunning = true;
+
 function setup() {
   let sketch = createCanvas(600, 400);
 }
@@ -18,7 +19,8 @@ function draw() {
   fill(255, 255, 0);
   textSize(44);
   text("Welcome to CS20!", textX, 150);
-  if (isRunning == true) {
+  //animation, move to the left
+  if (isRunning == true) {  
     textX = textX - 2;
 
     if (textX < -250) {
