a4c1de7c99ddab74a4aa5f65c78f31f6f3759b9a
Andreas.Home <byte.it@hotmail.com>
2022-04-10 20:05:19 -0500

    practice 1

---
+++ Teacher/Unit_1/Unit_1_practice_1/sketch.js
@@ -0,0 +1,22 @@
+/*
+* CS 20
+* Unit 1
+* Practise 1
+*/
+
+/** Some intro text */
+
+
+function setup() {
+  let numbers = [2,3,5,6];
+  numbers.forEach(num => {
+    testEvenOdd(num);
+  });
+
+}
+
+function testEvenOdd(number) {
+  //using an iff statement, check if the variable 'number' is even...
+  //print out the result using the print statement
+  
+}
